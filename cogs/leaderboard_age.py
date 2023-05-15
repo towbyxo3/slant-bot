7ca4461116d92892345ca387ebcb3b31c738b96c
towbyxo3 <tobi.69.slayer@gmail.com>
2023-05-15 16:02:10 +0000

    Overworked age leaderboard. Added sorting functionality button

--- cogs/leaderboard_age.py
+++ cogs/leaderboard_age.py
@@ -9,100 +9,163 @@ from helpers.dateformatting import format_YMD_to_DMY
 sys.path.append("helpers")
 
 
-class Age(discord.ui.View):
-    def __init__(self, ctx, num):
-        super().__init__()
-        self.ctx = ctx
-        self.num = num
+class AgeView(discord.ui.View):
+    """
+    View that displays a leaderboard of the server members based on their
+    - registration date
+    - join date in a server
+    It provides buttons for sorting the leaderboard by oldest and youngest
+    and toggling between the 2 base criteria (registration or join date).
+    """
+
+    # representing the current page of the leaderboard
+    # page 1 for registration date
+    # page 2 for join date
+    current_page: int = 1
+    # sort_by_youngest = False -> Sort by oldest
+    # sort_by_youngest = True -> Sort by youngest
+    sort_by_youngest = False
+
+    async def send(self, ctx):
+        """
+        Send the view to a channel. It creates a message
+        containing the view and updates it.
+        """
+        self.message = await ctx.send(view=self)
+        await self.update_message()
+
+    def create_embed(self):
+        """
+        Gathers the leaderboard data based on pressed buttons and creates embed.
+        """
+        if self.current_page == 1:
+            true_member_count = len([m for m in self.ctx.guild.members if not m.bot])
+            member_list = {}
+            guild = self.ctx.guild
+            for member in guild.members:
+                if not member.bot:
+                    member_name = member.id
+                    member_register = str(member.created_at)[:16]
+                    member_list[member_name] = member_register
+            sorted_list = sorted(member_list.items(), key=lambda x: x[1], reverse=self.sort_by_youngest)
+
+            embed = discord.Embed(
+                title=f"{'Youngest' if self.sort_by_youngest else 'Oldest'} Discord Users in {guild.name}",
+                timestamp=self.ctx.message.created_at,
+                color=discord.Color.red()
+            )
+            embed.set_author(
+                name=f"{guild.name} Leaderboard",
+                icon_url=guild.icon
+            )
+            embed.set_thumbnail(url=guild.icon)
+
+            leaderboard_text = ""
+            rank = 1
+            for data in sorted_list[:self.num]:
+                date = format_YMD_to_DMY(data[1][:10])
+                user = f"<@{data[0]}>"
+                leaderboard_text += f"`{rank}.` | {user} {date}\n"
+                rank += 1
+
+            embed.add_field(
+                name="Rank | Member | Created on",
+                value=leaderboard_text,
+                inline=False
+            )
+            embed.set_footer(text=f"{rank-1} out of {true_member_count} Members")
+        elif self.current_page == 2:
+            true_member_count = len([m for m in self.ctx.guild.members if not m.bot])
+            member_list = {}
+            guild = self.ctx.guild
+            for member in guild.members:
+                if not member.bot:
+                    member_name = member.id
+                    member_register = str(member.joined_at)[:16]
+                    member_list[member_name] = member_register
+            sorted_list = sorted(member_list.items(), key=lambda x: x[1], reverse=self.sort_by_youngest)
+
+            embed = discord.Embed(
+                title=f"{'Youngest' if self.sort_by_youngest else 'Oldest'} {guild.name} Members",
+                timestamp=self.ctx.message.created_at,
+                color=discord.Color.red()
+            )
+            embed.set_author(
+                name=f"{guild.name} Leaderboard",
+                icon_url=guild.icon
+            )
+            embed.set_thumbnail(url=guild.icon)
+
+            leaderboard_text = ""
+            rank = 1
+            for data in sorted_list[:self.num]:
+                date = format_YMD_to_DMY(data[1][:10])
+                user = f"<@{data[0]}>"
+                leaderboard_text += f"`{rank}.` | {user} {date}\n"
+                rank += 1
+
+            embed.add_field(
+                name="Rank | Member | Joined on",
+                value=leaderboard_text,
+                inline=False
+            )
+            embed.set_footer(text=f"{rank-1} out of {true_member_count} Members")
+
+        return embed
+
+    async def update_message(self):
+        """
+        Updates the message with the current state of the view.
+        It updates the buttons and the embed.
+        """
+        self.update_buttons()
+        await self.message.edit(embed=self.create_embed(), view=self)
+
+    def update_buttons(self):
+        """
+        Updates the state of the buttons based on the current page
+        page 1 - leaderboard sorted by registration date
+        page 2 - leaderboard sorted by join date
+        """
+        if self.current_page == 1:
+            self.registration.disabled = True
+            self.join.disabled = False
+            self.registration.style = discord.ButtonStyle.gray
+            self.join.style = discord.ButtonStyle.blurple
+
+        if self.current_page == 2:
+            self.registration.disabled = False
+            self.join.disabled = True
+            self.registration.style = discord.ButtonStyle.blurple
+            self.join.style = discord.ButtonStyle.gray
+
+        self.reverse.label = "Oldest" if self.sort_by_youngest else "Youngest"
 
     @discord.ui.button(label="Registration Date", style=discord.ButtonStyle.blurple)
-    async def Registration(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def registration(self, interaction: discord.Interaction, button: discord.ui.Button):
         """
         Returns the oldest discord users of the server.
         """
-
-        true_member_count = len([m for m in self.ctx.guild.members if not m.bot])
-        member_list = {}
-        guild = self.ctx.guild
-        for member in guild.members:
-            if not member.bot:
-                member_name = member.id
-                member_register = str(member.created_at)[:16]
-                member_list[member_name] = member_register
-        sorted_list = sorted(member_list.items(), key=lambda x: x[1])
-
-        embed = discord.Embed(
-            title=f"Oldest Discord Users in {guild.name}",
-            timestamp=self.ctx.message.created_at,
-            color=discord.Color.red()
-        )
-        embed.set_author(
-            name=f"{guild.name} Leaderboard",
-            icon_url=guild.icon
-        )
-        embed.set_thumbnail(url=guild.icon)
-
-        leaderboard_text = ""
-        rank = 1
-        for data in sorted_list[:self.num]:
-            date = format_YMD_to_DMY(data[1][:10])
-            user = f"<@{data[0]}>"
-            leaderboard_text += f"`{rank}.` | {user} {date}\n"
-            rank += 1
-
-        embed.add_field(
-            name="Rank | Member | Created on",
-            value=leaderboard_text,
-            inline=False
-        )
-        embed.set_footer(text=f"{rank-1} out of {true_member_count} Members")
-
-        await interaction.message.edit(embed=embed)
+        self.current_page = 1
+        await self.update_message()
         await interaction.response.defer()
 
     @discord.ui.button(label="Join Date", style=discord.ButtonStyle.blurple)
-    async def Join(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def join(self, interaction: discord.Interaction, button: discord.ui.Button):
         """
         Returns the oldest members of the server.
         """
+        self.current_page = 2
+        await self.update_message()
+        await interaction.response.defer()
 
-        true_member_count = len([m for m in self.ctx.guild.members if not m.bot])
-        member_list = {}
-        guild = self.ctx.guild
-        for member in guild.members:
-            if not member.bot:
-                member_name = member.id
-                member_register = str(member.joined_at)[:16]
-                member_list[member_name] = member_register
-        sorted_list = sorted(member_list.items(), key=lambda x: x[1])
-
-        embed = discord.Embed(
-            title=f"Oldest {guild.name} Members",
-            timestamp=self.ctx.message.created_at,
-            color=discord.Color.red()
-        )
-        embed.set_author(
-            name=f"{guild.name} Leaderboard",
-            icon_url=guild.icon
-        )
-        embed.set_thumbnail(url=guild.icon)
-
-        leaderboard_text = ""
-        rank = 1
-        for data in sorted_list[:self.num]:
-            date = format_YMD_to_DMY(data[1][:10])
-            user = f"<@{data[0]}>"
-            leaderboard_text += f"`{rank}.` | {user} {date}\n"
-            rank += 1
-
-        embed.add_field(
-            name="Rank | Member | Joined on",
-            value=leaderboard_text,
-            inline=False
-        )
-        embed.set_footer(text=f"{rank-1} out of {true_member_count} Members")
-
-        await interaction.message.edit(embed=embed)
+    @discord.ui.button(label="Youngest", style=discord.ButtonStyle.blurple)
+    async def reverse(self, interaction: discord.Interaction, button: discord.ui.Button):
+        """
+        Toggles sort order.
+        """
+        self.sort_by_youngest = not self.sort_by_youngest
+        await self.update_message()
         await interaction.response.defer()
 
 
@@ -115,23 +178,15 @@ class AgeLeaderboard(commands.Cog):
     @commands.command(alises=["oldest", "ageleaderboard", "agelb"])
     async def age(self, ctx, num=10):
         """
-        Oldest Users in Server by Registration Date and Server Join Date
+        Oldest or youngest members in Server by
+        - Discord Registration Date
+        - Server Join Date
+        .
         """
-
-        true_member_count = len([m for m in ctx.guild.members if not m.bot])
-
-        embed = discord.Embed(
-            color=discord.Color.blue(),
-            timestamp=ctx.message.created_at,
-            title="Select Age Criteria",
-            description=f"Oldest {ctx.guild.name} Members by Registration Date or Join Date"
-        )
-        embed.set_thumbnail(url=ctx.guild.icon)
-        embed.set_footer(
-            text=f"Server ID: {ctx.guild.id} | {true_member_count} Members"
-        )
-
-        await ctx.send(embed=embed, view=Age(ctx, num))
+        pagination_view = AgeView(timeout=120)
+        pagination_view.num = num
+        pagination_view.ctx = ctx
+        await pagination_view.send(ctx)
 
 
 async def setup(bot):
